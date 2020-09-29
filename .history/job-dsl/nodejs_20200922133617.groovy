46413e432fa3fddd782fd29b4061ff07701cdd28
ransolo <ransolo@me.com>
2020-09-29 09:56:31 -0500

    update-node-name

--- .history/job-dsl/nodejs_20200922133617.groovy
+++ .history/job-dsl/nodejs_20200922133617.groovy
@@ -1,4 +1,4 @@
-job('NodeJS Docker example') {
+job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
@@ -9,18 +9,10 @@ job('NodeJS Docker example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('NodeJS') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
-            forcePull(false)
-            forceTag(false)
-            createFingerprints(false)
-            skipDecorate()
-        }
+        shell("npm install")
     }
 }
